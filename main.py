acab3ab2da9027d5ba36add4ab407fab81f27af5
santoshmc24157 <santoshm_c24157@students.isquareit.edu.in>
2026-09-07 06:33:03 +0000

    Python Program

---
+++ main.py
@@ -0,0 +1,10 @@
+name = input("Enter your name: ")
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Hello", name)
+print("Addition =", a + b)
+print("Subtraction =", a - b)
+print("Multiplication =", a * b)
+print("Division =", a / b)
